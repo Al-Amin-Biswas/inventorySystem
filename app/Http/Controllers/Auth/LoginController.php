47ab6b8fa147a70e1905e4f7241d567176ffb637
Al-Amin-Biswas <mdmin24@gmail.com>
2022-03-29 15:32:57 +0600

    Project initializtion

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,21 +1,33 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
+
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
     use AuthenticatesUsers;
+
     /**
      * Where to redirect users after login.
      *
      * @var string
      */
-    //protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = RouteServiceProvider::HOME;
+
     /**
      * Create a new controller instance.
      *
@@ -24,14 +36,5 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-    public function gotoPage(){
-        return view('backend/dashboard');
-    }
-    public function UserCheak(Request $request){
-        $credentials = $request->only('email', 'password');
-        if (Auth::attempt($credentials)) {
-            return redirect()->intended('/dash');
-        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/', function () {
     return view('welcome');
 });
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('testCheck','auth\LoginController@gotoPage');
-Route::post('checkUser','auth\LoginController@UserCheak');
-//----Menu section
-Route::get('menu_bar','MenuList\MenuController@index');
-Route::get('add_menu','MenuList\MenuController@addMenu');
-Route::post('adding_menu','MenuList\MenuController@addingMenu');
-Route::get('edit/menu/{id}','MenuList\MenuController@editMenu');
-Route::post('update_menu{id}','MenuList\MenuController@menuUpdate');
-Route::get('delete/menu{id}','MenuList\MenuController@menuDelete');
-//---Sub Menu Section
-Route::get('sub_menu','MenuList\MenuController@subIndex');
-Route::get('add_sub_menu','MenuList\MenuController@addSubmenu');
-Route::post('adding_Submenu','MenuList\MenuController@addingsubMenu');
-Route::post('all_Submenu','MenuList\MenuController@AllsubMenu');
